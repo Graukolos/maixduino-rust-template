55c2c04e211914839d0ba3a1b0cb064b7a0396ed
Max Erdelmeier <erdelmei@rhrk.uni-kl.de>
2022-05-31 17:02:59 +0200

    Fix stupidness

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 cargo build --release
 
-kflash -p /dev/tty.usbserial-xel_sipeed0 -B maixduino -s target/riscv64gc-unknown-none-elf/release/maixduino-test
+kflash -p /dev/tty.usbserial-xel_sipeed0 -B maixduino -s target/riscv64gc-unknown-none-elf/release/{{project-name}}
